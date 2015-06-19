7952ddbbc2a6cf93b016fc9cf8b1cf12710d5c9c
高田早記 <abare.sy@gmail.com>
2015-06-19 22:31:30 +0900

    first commit

--- Count_practice_6/ViewController.swift
+++ Count_practice_6/ViewController.swift
@@ -5,10 +5,15 @@
 //  Created by 高田早記 on 2015/06/19.
 //  Copyright (c) 2015年 高田早記. All rights reserved.
 //
+//2分35秒
+//中級編　+5分33秒
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number:Int = 0
+    @IBOutlet var label:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+        
+        if number>0 {
+            label.textColor = UIColor.redColor()
+        }else if number<0 {
+            label.textColor = UIColor.blueColor()
+        }else {
+            label.textColor = UIColor.blackColor()
+        }
+    }
+    
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+        
+        if number>0 {
+            label.textColor = UIColor.redColor()
+        }else if number<0 {
+            label.textColor = UIColor.blueColor()
+        }else {
+            label.textColor = UIColor.blackColor()
+        }
+    }
+    
+    
 
 }
 
