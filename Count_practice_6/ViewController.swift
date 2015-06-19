6a8d1a59b06f4c3878bef5b3ccd112a3a3478159
高田早記 <abare.sy@gmail.com>
2015-06-19 22:12:24 +0900

    Initial Commit

---
+++ Count_practice_6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Count_practice_6
+//
+//  Created by 高田早記 on 2015/06/19.
+//  Copyright (c) 2015年 高田早記. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
